91898a5631a8e37876ae324bad94bdce1360c38b
Sergey Burenkov <uzaren@ukr.net>
2025-11-22 11:43:28 +0200

    OZON Lesson 4 + Cart counter

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,12 +1,24 @@
+import renderCart from "./renderCart";
+import postData from "./postData";
+
 const cart = () => {
     const cartBtn = document.getElementById('cart');
     const cartModal = document.querySelector('.cart');
     const cartCloseBtn = cartModal.querySelector('.cart-close')
-
-
+    const goods = document.querySelector('.goods');
+    const cartTotal = cartModal.querySelector('.cart-total > span');
+    const cartWrapper = document.querySelector('.cart-wrapper');
+    const cartSendBtn = cartModal.querySelector('.cart-confirm');
+    const cartCnt = cartBtn.querySelector('.counter');
 
     const openCart = () => {
         cartModal.style.display = 'flex'
+        const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+        cartCnt.textContent = cart.length;
+        renderCart(cart);
+        cartTotal.textContent = cart.reduce((sum, goodItem) => {
+            return sum + goodItem.price
+        }, 0)
     }
 
     const closeCart = () => {
@@ -17,7 +29,53 @@ const cart = () => {
 
     cartCloseBtn.addEventListener('click', closeCart);
 
+    goods.addEventListener('click', (event) => {
+        if (event.target.classList.contains('btn-primary')) {
+            const card = event.target.closest('.card');
+            const key = card.dataset.key;
+            const goods = JSON.parse(localStorage.getItem('goods'));
+            const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+            const goodItem = goods.find((item) => {
+                return item.id === key;
+            })
+            cart.push(goodItem);
+            localStorage.setItem('cart', JSON.stringify(cart));
+            cartCnt.textContent = cart.length;
+        }
+    })
 
+    cartWrapper.addEventListener('click', (event) => {
+        if (event.target.classList.contains('btn-primary')) {
+            const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+
+            const card = event.target.closest('.card');
+            const key = card.dataset.key;
+            const index = cart.findIndex((item) => {
+                item.id === +key;
+            })
+            cart.splice(index, 1)
+            localStorage.setItem('cart', JSON.stringify(cart));
+            renderCart(cart);
+            cartCnt.textContent = cart.length;
+            cartTotal.textContent = cart.reduce((sum, goodItem) => {
+                return sum + goodItem.price
+            }, 0)
+
+        }
+
+    })
+
+    cartSendBtn.addEventListener('click', () => {
+        const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+
+        postData(cart).then(() => {
+            localStorage.removeItem('cart');
+            renderCart([]);
+            cartTotal.textContent = 0;
+            cartCnt.textContent = 0;
+        })
+
+    })
 
 }
 
--- src/modules/filters.js
+++ src/modules/filters.js
@@ -10,3 +10,33 @@ export const categoryFilter = (goods, value) => {
         return goodsItem.category === value;
     })
 }
+
+export const priceFilter = (goods, min, max) => {
+    return goods.filter((goodsItem) => {
+        if (min === '' && max === '') {
+            return goodsItem;
+        }
+        if (min !== '' && max !== '') {
+            return goodsItem.price > +min && goodsItem.price < +max;
+        }
+        if (min !== '' && max === '') {
+            return goodsItem.price > +min;
+        }
+        if (min === '' && max !== '') {
+            return goodsItem.price < +max;
+        }
+
+    })
+}
+
+export const hotSaleFilter = (goods, value) => {
+    return goods.filter((goodsItem) => {
+        return value ? goodsItem.sale === true : goodsItem;
+        // if (value) {
+        //     return goodsItem.sale === true;
+        // }
+        // else {
+        //     return goodsItem;
+        // };
+    })
+}
